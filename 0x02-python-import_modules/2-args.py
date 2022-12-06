158164044cdf269d6162222dfee799f3b714cdaa
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2022-12-06 08:56:30 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    x = len(sys.argv) - 1
-    print("{} arguement".format(x), end="")
-    if x == 0:
-        print("s.")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        if x > 1:
-            print("s", end="")
-            print(":")
-            for y in range(1, x + 1):
-                print("{}: {}".format(y, sys.argv[y]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
